a5bedc0ff26af6a52d8010d8ed10849a6d00467a
Michael Dombrowski <Michael@mikedombrowski.com>
2017-08-24 21:06:35 -0400

    Add hyphen and underscore and option to clear display
    Add hyphen and underscore, option to clear display or persist, and right
    shift. Added field to hold the bytes currently on the display. Changed
    constructor to add the digitLimit in the constructor.

--- src/DigitLedDisplay.cpp
+++ src/DigitLedDisplay.cpp
@@ -7,7 +7,7 @@
 #define DISPLAYTEST_ADDR 15
 
 
-DigitLedDisplay::DigitLedDisplay(int dinPin, int csPin, int clkPin) {
+DigitLedDisplay::DigitLedDisplay(int dinPin, int csPin, int clkPin, int digitLimit) {
 	DIN_PIN = dinPin;
 	CS_PIN = csPin;
 	CLK_PIN = clkPin;
@@ -16,6 +16,14 @@ DigitLedDisplay::DigitLedDisplay(int dinPin, int csPin, int clkPin) {
 	pinMode(CS_PIN, OUTPUT);
 	pinMode(CLK_PIN, OUTPUT);
 	digitalWrite(CS_PIN, HIGH);
+	
+	setDigitLimit(digitLimit);
+}
+
+DigitLedDisplay::~DigitLedDisplay(){
+	if(currentDisplayState){
+		delete[] currentDisplayState;
+	}
 }
 
 void DigitLedDisplay::setBright(int brightness) {
@@ -25,6 +33,7 @@ void DigitLedDisplay::setBright(int brightness) {
 
 void DigitLedDisplay::setDigitLimit(int limit) {
 	_digitLimit = limit;
+	currentDisplayState = new byte[_digitLimit];
 
 	write(DISPLAYTEST_ADDR, 0);
 	write(SCANLIMIT_ADDR, limit-1);
@@ -46,14 +55,15 @@ void DigitLedDisplay::off() {
 }
 
 void DigitLedDisplay::clear() {
-  for (int i = 1; i <=_digitLimit; i++) {
-	write(i, B00000000);
+  for (int i = 1; i <= _digitLimit; i++) {
+	writeDigit(i, DIGIT_BLANK);
+	currentDisplayState[i - 1] = DIGIT_BLANK;
   }
 }
 
 void DigitLedDisplay::table(byte address, int val) {
 	byte tableValue = pgm_read_byte_near(charTable + val);
-	write(address, tableValue);
+	writeDigit(address, tableValue);
 }
 
 void DigitLedDisplay::write(volatile byte address, volatile byte data) {
@@ -61,6 +71,12 @@ void DigitLedDisplay::write(volatile byte address, volatile byte data) {
 	shiftOut(DIN_PIN, CLK_PIN, MSBFIRST, address);
 	shiftOut(DIN_PIN, CLK_PIN, MSBFIRST, data);
 	digitalWrite(CS_PIN, HIGH);
+}
+
+void DigitLedDisplay::writeDigit(volatile byte address, volatile byte data) {
+	address = (address % _digitLimit) + 1;
+	write(address, data);
+	currentDisplayState[address - 1] = data;
 }
 
 void DigitLedDisplay::printDigit(long number, byte startDigit) {
@@ -77,41 +93,76 @@ void DigitLedDisplay::printDigit(long number, byte startDigit) {
 	}
 }
 
-void DigitLedDisplay::printDigitAndPeriod(String number) {
+void DigitLedDisplay::printText(String number, int rightShift, bool clearDisplay) {
 	int figureLength = number.length();
 
 	int parseInt;
 	char str[2];
+	
 	byte buffer[_digitLimit];
-	for(int i=0; i<_digitLimit; i++){
-		buffer[i] = B00000000;
+	for(int i = 0; i < _digitLimit; i++){
+		if(clearDisplay){
+			buffer[_digitLimit - i - 1] = DIGIT_BLANK;
+			continue;
+		}
+		buffer[_digitLimit - i - 1] = currentDisplayState[i];
 	}
+	
 	int printedDigits = 0;
+	int spacesInRow = 0;
 	for(int i = 0; i < figureLength; i++) {
+		int digitIndex = (printedDigits + rightShift) % _digitLimit;
+		if(digitIndex < 0){
+			digitIndex = _digitLimit + digitIndex;
+		}
+		
 		if(number[i] != '.'){
+			
 			if(number[i] == ' '){
-				buffer[printedDigits] = B00000000;
+				spacesInRow++;
+				buffer[digitIndex] = DIGIT_BLANK;
+				if(spacesInRow % 2 == 0){
+					printedDigits++;
+				}
+				continue;
+			}
+			else if(number[i] == '-'){
+				buffer[digitIndex] = DIGIT_HYPHEN;
 				printedDigits++;
 				continue;
 			}
+			else if(number[i] == '_'){
+				buffer[digitIndex] = DIGIT_UNDERSCORE;
+				printedDigits++;
+				continue;
+			}
+			
+			spacesInRow = 0;
 			str[0] = number[i];
 			str[1] = '\0';
 			parseInt = (int) strtol(str, NULL, 10);
 			byte tableValue = pgm_read_byte_near(charTable + parseInt);
-			buffer[printedDigits] = tableValue;
+			buffer[digitIndex] = tableValue;
 			printedDigits++;
 		}
-		else if(printedDigits > 0 && number[i - 1] != '.'){
-			buffer[printedDigits - 1] = buffer[printedDigits - 1] | B10000000;
+		// Add period to previous digit
+		else if(printedDigits > 0 && number[i - 1] != ' ' && number[i - 1] != '.'){
+			if(digitIndex < 1){
+				digitIndex = (printedDigits - 1) % _digitLimit;
+			}
+			else {
+				digitIndex--;
+			}
+			buffer[digitIndex] = buffer[digitIndex] | DIGIT_PERIOD;
 		}
+		// Add period alone
 		else {
-			buffer[printedDigits] = B10000000;
+			buffer[digitIndex] = DIGIT_PERIOD;
 			printedDigits++;
 		}
-		Serial.println(printedDigits);
 	}
 	
-	for(int i=0; i < _digitLimit; i++){
-		write(i+1, buffer[_digitLimit - i - 1]);
+	for(int i = 0; i < _digitLimit; i++){
+		writeDigit(i, buffer[_digitLimit - i - 1]);
 	}
 }
